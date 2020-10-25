b5f7d7dfb18fc5103936a034b9d95b89b1582cb9
Tkachenko Anton <tkachenko.anton.90@gmail.com>
2020-10-25 16:18:21 +0200

    добавляет условия 1 задачи

---
+++ js/task-1.js
@@ -0,0 +1,13 @@
+const getUserNames = array => array;
+
+// console.log(getUserNames(users));
+
+/* [
+  "Moore Hensley",
+  "Sharlene Bush",
+  "Ross Vazquez",
+  "Elma Head",
+  "Carey Barr",
+  "Blackburn Dotson",
+  "Sheree Anthony",
+] */

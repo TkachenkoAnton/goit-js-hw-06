d147204a8f603d7ea9ca61d15534ad72e5490f3e
Tkachenko Anton <tkachenko.anton.90@gmail.com>
2020-10-26 17:06:30 +0200

    добавляет комментарий к 4 задаче

--- js/task-4.js
+++ js/task-4.js
@@ -1,5 +1,5 @@
 // Функция getInactiveUsers получает массив как параметр
-// вызывем функцию filter  которая принмает как параметр свойство isActive
+// вызывем функцию filter  которая принмает как параметр свойство isActive с помощью ДП
 // если false пушит объект в массив
 
 const getInactiveUsers = array => array.filter(({ isActive }) => !isActive);

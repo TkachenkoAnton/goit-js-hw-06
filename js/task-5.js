6a27de7e140467d7dce601d68934f7ace4eeee3c
Tkachenko Anton <tkachenko.anton.90@gmail.com>
2020-10-26 17:06:13 +0200

    добавляет решение 5 задачи

--- js/task-5.js
+++ js/task-5.js
@@ -1,3 +1,7 @@
-const getUserWithEmail = (array, mail) => array;
+// Функция getUserWithEmail получает массив как параметр
+// вызывем функцию find которая принмает как параметр свойство email с помощью ДП
+// и сравнивает значение из свойства с параметром функции getUserWithEmail
+
+const getUserWithEmail = (array, mail) => array.find(({ email }) => email === mail);
 
 // console.log(getUserWithEmail(users, 'rossvazquez@xinware.com'));

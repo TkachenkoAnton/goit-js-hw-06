1f4b49da1f22d468888851917ad52858b0903bea
Tkachenko Anton <tkachenko.anton.90@gmail.com>
2020-10-27 19:14:12 +0200

    добавляет решение 9 задачи

---
+++ js/task-9.js
@@ -0,0 +1,12 @@
+// Функция getNamesSortedByFriendsCount получает массив как параметр
+// делаем копию массива, так как метод sort мутирует исходный массив
+// вызывем метод sort в кторой прописываем параметры для сортировки по возростанию
+// и обрщаемся к значению длинны свойства friends, так как метод sort умеет работать только с числами  
+// на результате вызываем метод мар возвращаем результат - массив имен
+
+const getNamesSortedByFriendsCount = array =>
+    [...array]
+        .sort((prev, next) => prev.friends.length - next.friends.length)
+        .map(({name}) => name);
+
+console.log(getNamesSortedByFriendsCount(users));

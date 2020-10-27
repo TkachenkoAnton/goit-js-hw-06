57ce891d976a622875bf38cf49e567a18357d255
Tkachenko Anton <tkachenko.anton.90@gmail.com>
2020-10-27 17:48:15 +0200

    добавляет условие 8 задачи

---
+++ js/task-8.js
@@ -0,0 +1,7 @@
+const getUsersWithFriend = (array, friendName) => array;
+
+// console.log(getUsersWithFriend(users, 'Briana Decker'));
+// [ 'Sharlene Bush', 'Sheree Anthony' ]
+
+// console.log(getUsersWithFriend(users, 'Goldie Gentry'));
+// [ 'Elma Head', 'Sheree Anthony' ]

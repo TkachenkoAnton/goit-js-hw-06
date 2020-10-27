9fe41f097bbe9b020fe547d3c65a8747ea2ad828
Tkachenko Anton <tkachenko.anton.90@gmail.com>
2020-10-27 18:14:30 +0200

    добавляет решение 8 задачи

--- js/task-8.js
+++ js/task-8.js
@@ -1,4 +1,12 @@
-const getUsersWithFriend = (array, friendName) => array;
+// Функция getUsersWithFriend получает массив и имя друга как параметр
+// вызывем метод filter в кторой прописываем свойство friends с помощью ДП
+// и проверяем на налачие параметра friendName
+// на результате вызываем метод мар возвращаем результат
+
+const getUsersWithFriend = (array, friendName) =>
+        array
+        .filter(({friends}) => friends.includes(friendName))
+        .map(({name}) => name);
 
 // console.log(getUsersWithFriend(users, 'Briana Decker'));
 // [ 'Sharlene Bush', 'Sheree Anthony' ]

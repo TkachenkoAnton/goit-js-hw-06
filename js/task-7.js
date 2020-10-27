969eaccd2f5f15b181473d013380a0c7614887dc
Tkachenko Anton <tkachenko.anton.90@gmail.com>
2020-10-27 17:48:33 +0200

    добавляет условие 7 задачи

--- js/task-7.js
+++ js/task-7.js
@@ -1,3 +1,8 @@
- const calculateTotalBalance = array => array;
+// Функция alculateTotalBalance получает массив как параметр
+// вызывем функцию reduce  в кторой прописываем параметр счетчика и параметр свойство balance с помощью ДП
+// плюсуем все значения свойства balance со счетчиком который начинается с 0
+// возвращаем результат
+
+const calculateTotalBalance = array => array.reduce((acc, { balance }) => acc + balance, 0);
 
 // console.log(calculateTotalBalance(users)); 

17f45fb214b3620a0bbe46e75e9457d2bf7311f2
Tkachenko Anton <tkachenko.anton.90@gmail.com>
2020-10-28 00:23:16 +0200

    добавляет решение 10 задачи

--- js/task-10.js
+++ js/task-10.js
@@ -1,12 +1,13 @@
-// const getSortedUniqueSkills = array =>
-//     array
-//         .reduce((skillsArr, skill) => {
-//             skillsArr.push(...skill.skills)
+// Функция getSortedUniqueSkills получает массив как параметр
+// вызываем метод reduce, его параметры счетчик и свойство skills в теле метода делаем Слияние массивов
+// на результате вызываем метод filter где обьявляем параметры текущего елемента, индекса и весь массив
+// ищем в массиве елемент по индексу если такой эть он попадает в массив 
+// на результате вызываем метод sort
 
-//             return skillsArr
-
-//         }, [])
-//         .filter((skill, i, allSkills) => allSkills.splice(allSkills.indexOf(skill), 1))
-//         .sort();
+const getSortedUniqueSkills = array =>
+    array
+        .reduce((accSkills, {skills}) => [...accSkills, ...skills], [])
+        .filter((skill, i, skills) => skills.indexOf(skill) === i)
+        .sort();
 
 // console.log(getSortedUniqueSkills(users));
